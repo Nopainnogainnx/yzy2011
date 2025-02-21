5fc4a4a666e7429c9bef911be2e4a9b83cb339fe
Nopainnogainnx <1243502302@qq.com>
2025-02-21 13:33:08 +0800

    Create Num4.html

---
+++ Num4.js
@@ -0,0 +1,13 @@
+<!-- dashboard.html -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>数据统计</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <h1>访问统计</h1>
+    <canvas id="statsChart"></canvas>
+    <script src="dashboard.js"></script>
+</body>
+</html>
